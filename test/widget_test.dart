89906ae6e998016cc7b7aeade873e66ca4d6ae98
Steven Deseure <deseure.s@gmail.com>
2020-11-18 14:04:27 +0100

    pubspec strangerThings

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:deseure_steven_card/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MyBusinessCard());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
